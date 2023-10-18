294f843f48adf357a92d8f4f72b643f84420b7be
oneimu <62015433+Oneimu@users.noreply.github.com>
2023-10-18 10:18:52 -0500

    frontend

---
+++ src/component/homepage/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {AppBar} from '@mui/icons-material'
+
+const Header = () => {
+    return (
+        
+        <div>
+            Hey You!
+        </div>
+    )
+}
+
+export default Header;
